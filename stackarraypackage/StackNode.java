f7fb53b3bae5e51812d88958580bd222e91f841f
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-09-30 23:45:37 +0530

    stackarray

---
+++ stackarraypackage/StackNode.java
@@ -0,0 +1,10 @@
+package stackarraypackage;
+public class StackNode {
+  public StackNode above;
+  public StackNode below;
+  public int value;
+  
+  public StackNode(int value) {
+    this.value = value;
+  }
+}
