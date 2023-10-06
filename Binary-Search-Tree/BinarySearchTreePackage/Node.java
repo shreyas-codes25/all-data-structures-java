7b3843e188b8f8b7b655838362c571dac65354d8
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-06 23:01:02 +0530

    Add files via upload

---
+++ Binary-Search-Tree/BinarySearchTreePackage/Node.java
@@ -0,0 +1,10 @@
+package BinarySearchTreePackage;
+class Node{
+    int data;
+    Node left,right;
+    public Node(int data){
+        this.data=data;
+        this.right=null;
+        this.left=null;
+    }
+} 
