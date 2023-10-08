4cdaf27467e41b47e608b889df2984bcbd76b42e
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-08 19:28:36 +0530

    Update Main.java

--- graphCollege/Main.java
+++ graphCollege/Main.java
@@ -3,15 +3,21 @@ package graphCollege;
 public class Main {
     public static void main(String agrs[]){
         Graph g= new Graph(6);
+        
+        g.addEdge(0, 1);
+        g.addEdge(0, 3);
+        g.addEdge(0, 4);
         g.addEdge(4, 5);
         g.addEdge(3, 5);
         g.addEdge(1, 2);
+        g.addEdge(1, 0);
         g.addEdge(2, 1);
         g.addEdge(4, 1);
         g.addEdge(3, 1);
         g.addEdge(5, 4);
+        g.addEdge(5, 3);
         System.out.println("Traversal");
-        g.BFS(2);
+        g.bfs(0);
     }
     
 }
