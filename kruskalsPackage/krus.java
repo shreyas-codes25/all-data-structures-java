672ca3b6f107d03c75da71b8ee4780bad813586d
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-16 22:37:44 +0530

    Update krus.java

--- kruskalsPackage/krus.java
+++ kruskalsPackage/krus.java
@@ -1,74 +1,74 @@
 package kruskalsPackage;
 
-public class krus {
-    public static int V;
-   static  int[] parent;
-    static int INF=Integer.MAX_VALUE;
-    int edges[][] ;
-    krus(int v,int graph[][]){
-        krus.V=v;
-        parent=new int[V];
-        edges= graph;
+public class krus{
+    static int V;
+    static int parent[];
+    int edges[][];
+    int INF=Integer.MAX_VALUE;
 
+    public krus(int vertex,int weights[][]){
+        krus.V=vertex;
+        this.edges=weights;
+        parent = new int[V];
     }
-
     static int find(int i){
         while(parent[i]!=i){
             i=parent[i];
+            System.out.print(i);
         }
         return i;
     }
 
-    static void unoin(int a,int b){
-        int first = find(a);
-        int second = find(b);
-
-        parent[first]=second;
+    public void union(int first,int second){
+        int a =find(first);
+        int b=find(second);
+        parent[a]=b;
     }
-    static void kruskalMST(int graph[][]){
+
+    public void krusCall(){
+        krusMST(edges);
+    }
+
+    public void krusMST(int cost[][]){
+        int no_edges=0;
         int mincost=0;
 
         for(int i=0;i<V;i++){
             parent[i]=i;
+            
         }
 
-        int edge_count=0;
-        while(edge_count<V-1){
-            int min = INF;
+        while(no_edges<V-1){
+            int min=INF;
             int a=-1;
             int b=-1;
 
             for(int i=0;i<V;i++){
                 for(int j=0;j<V;j++){
-                    if(find(i)!=find(j) && graph[i][j] <min){
-                        min = graph[i][j];
+                    if(find(i) != find(j) && cost[i][j] < min){
+                        min =cost[i][j];
                         a=i;
                         b=j;
                     }
-
                 }
             }
-            unoin(a, b);
-            System.out.printf("Edge %d:(%d,%d)  : cost : %d\n",edge_count++,a,b,min);
+            union(a,b);
+            System.out.printf("EDGE %d(%d,%d) : weigth : %d\n",no_edges++,a,b,min);
             mincost+=min;
         }
-        System.out.printf("total cost = "+mincost);
+        System.out.println("Total minimum cost was : "+mincost);
     }
-    public void kruskalMSTCall(){
-        kruskalMST(edges);
+        public static void main(String args[]){
+        int INF=Integer.MAX_VALUE;
+        int weights[][]={{INF,2,INF,6,INF},
+                         {2,INF, 3, 8, 5},
+                         {INF,3,INF,INF,7},
+                         {6,8,INF, INF, 9},
+                         {INF,5,7,9,INF}};
+        krus k = new krus(5, weights);
+        k.krusCall();
     }
-    
-    public static void main(String args[]){
-        int cost[][]={{INF,2,INF,6,INF},
-                      {2,INF, 3, 8, 5},
-                      {INF,3,INF,INF,7},
-                      {6,8,INF, INF, 9},
-                      {INF,5,7,9,INF}};
-        krus g= new krus(5,cost);
-        g.kruskalMSTCall();
 
-
-
-    }
-    
 }
+
+
