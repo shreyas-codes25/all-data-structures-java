db6702c7cb03e206b260a084cadade6132fac80b
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-16 22:47:35 +0530

    update prims algorithm

--- primsPackage/Main.java
+++ primsPackage/Main.java
@@ -4,15 +4,16 @@ public class Main {
     
  public static void main(String[] args) {
     PGraph2 g = new PGraph2();
+    int INF=Integer.MAX_VALUE;
     // number of vertices in grapj
     int V = 5;
     // create a 2d array of size 5x5
     // for adjacency matrix to represent graph
-    int[][] G = { { 0, 9, 75, 0,  0 }, 
-	                { 9, 0, 95, 19, 42 }, 
-	                { 75,95, 0, 51, 66 }, 
-	                { 0, 19, 51, 0, 31 },
-                  { 0, 42, 66, 31, 0 } };
+    int[][] G = { { INF, 9, 75, INF,  INF }, 
+	                { 9, INF, 95, 19, 42 }, 
+	                { 75,95, INF, 51, 66 }, 
+	                { INF, 19, 51, INF, 31 },
+                  { INF, 42, 66, 31, INF } };
     g.primsalgo(G, V);
   }
     
--- primsPackage/PGraph2.java
+++ primsPackage/PGraph2.java
@@ -1,36 +1,33 @@
 package primsPackage;
 import java.util.Arrays;
-
 public class PGraph2{
-    public void primsalgo(int G[][],int V){
-        boolean visited[] =new boolean[V];
-        Arrays.fill(visited,false);
-        visited[0]=true;
+    public void primsalgo(int cost[][],int TV){
         int INF=Integer.MAX_VALUE;
-        int no_edge=0;
-        System.out.println("EDGES  :  WEIGHT");
+        int no_edges=0;
+        boolean visited[] = new boolean[TV];
+        Arrays.fill(visited,false);
 
-        while(no_edge <V-1){
+        visited[0]=true;
+        System.out.println("EDGES :: WEIGHT");
+        while(no_edges<TV-1){
             int min=INF;
-            int x=0;
-            int y=0;
+            int a=0;
+            int b=0;
 
-            for(int i=0;i<V;i++){
+            for(int i=0;i<TV;i++){
                 if(visited[i]){
-                    for(int j=0;j<V;j++){
-                        if(!visited[j] && G[i][j] !=0){
-                            if(min > G[i][j]){
-                                min = G[i][j];
-                                x=i;
-                                y=j;
-                            }
+                    for(int j=0;j<TV;j++){
+                        if(!visited[j] && cost[i][j]<min){
+                            min=cost[i][j];
+                            a=i;
+                            b=j;
                         }
                     }
                 }
             }
-            System.out.println(x+"  --  "+y+"  :  "+G[x][y]);
-            visited[y]=true;
-            no_edge++;
+            System.out.println(a+" - "+b+"  :  "+cost[a][b]);
+            visited[b]=true;
+            no_edges++;
         }
     }
 }
