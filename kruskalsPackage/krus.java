352e0430a2df4e99c9ecb09686e18594dca4d072
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-17 23:31:32 +0530

    Update krus.java

--- kruskalsPackage/krus.java
+++ kruskalsPackage/krus.java
@@ -5,13 +5,20 @@ public class krus{
     int v=5;
     int parent[] = new int[v];
 
+    // used to find the parent of the vertex asked for, 
+    //the find method searches if the element is present in the 
+    // connected list of the parent;
     int find(int i){
-        while(parent[i]!=i){
-            i=parent[i];
+        if(parent[i] == i){
+            return i;
         }
-        return i;
+        return find(parent[i]);
+        
     }
-
+    // if the parent of two elements is different then this method will join them together
+    // in the kruskal's algorithm case , the union method is used to join one vertex to another
+    // so that it'll form an edge therefore, connecting two vertices together so that those elements can get fixed in place
+    // & not called again , therefore not repreating the same edge. 
     void union(int a,int b){
         int x=find(a);
         int y=find(b);
@@ -23,15 +30,17 @@ public class krus{
         int no_edges=0;
         int mincost=0;
 
+        // initializing the total no. of elements with parent as them self,
+        // initially all are disjoint set.
         for(int i=0;i<v;i++){
             parent[i]=i;
         }
-
+        // till all the vertices are not connected
         while(no_edges<v-1){
             int min=Integer.MAX_VALUE;
             int a=0;
             int b=0;
-
+            //main functionality lies here.
             for(int i=0;i<v;i++){
                 for(int j=0;j<v;j++){
                     if(find(i)!=find(j) && cost[i][j] <min) {
@@ -41,6 +50,7 @@ public class krus{
                     }
                 }
             }
+            //joining two vertices together
             union(a, b);
             System.out.printf("Edge %d (%d,%d)  :weigth=%d\n",no_edges++,a,b,min);
             mincost+=min;
