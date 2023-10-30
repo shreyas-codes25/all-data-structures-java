eca97739d9ce5db2ad91cbb7cb6b060ce5e5b3a7
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-10-30 20:58:28 +0530

    commit

--- binarytreepackage/Execution.java
+++ binarytreepackage/Execution.java
@@ -1,7 +1,7 @@
 package binarytreepackage;
 
-public class BTreeExample {
-    public BTreeExample(){
+public class Execution {
+    public Execution(){
         BinaryTree2 s = new BinaryTree2();
         s.DisplayTree();
         System.out.println("\nTotal number of leaves in the given tree are ="+s.getLeafCount());
